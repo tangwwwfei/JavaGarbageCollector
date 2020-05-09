66958d2867711cb58dfb15cbdea49c0d1032e588
tangwwwfei <44647556+tangwwwfei@users.noreply.github.com>
2020-05-10 01:25:42 +0300

    Update GCTestScript.sh

--- GCTestScript.sh
+++ GCTestScript.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 GCs=( "Serial" "Parallel" "ConcMarkSweep" "G1" "Z" "Shenandoah" )
-test_options=( "fop" "tradesoap" "tradebeans" )
+test_options=( fop tradebeans tradesoap )
 for gc in ${GCs[@]}
 do
-	for option in ${test_options[@]}
-	do
-	sudo java -XX:+UnlockExperimentalVMOptions -XX:+Use${gc}GC -Xloggc:${gc}_${option}_gc.log -XX:+PrintGCDetails -jar dacapo-9.12-MR1-bach.jar ${option}
-	done
+        for option in ${test_options[@]}
+        do
+        sudo java -XX:+UnlockExperimentalVMOptions -XX:+Use${gc}GC -Xloggc:${gc}_${option}_gc.log -XX:+PrintGCDetails -verbose:gc -jar dacapo-9.12-MR1-bach.jar ${option}
+        done
 done
