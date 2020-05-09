61a947ff0dba8743f030681a3e57db970f58db9e
tangwwwfei <44647556+tangwwwfei@users.noreply.github.com>
2020-05-10 01:24:59 +0300

    Update GCTestScript.sh

--- GCTestScript.sh
+++ GCTestScript.sh
@@ -5,6 +5,6 @@ for gc in ${GCs[@]}
 do
 	for option in ${test_options[@]}
 	do
-	sudo java -XX:+UnlockExperimentalVMOptions -XX:+Use${gc}GC -Xlog:gc:${gc}_${option}_gc.log -XX:+PrintGCDetails -jar dacapo-9.12-MR1-bach.jar ${option}
+	sudo java -XX:+UnlockExperimentalVMOptions -XX:+Use${gc}GC -Xloggc:${gc}_${option}_gc.log -XX:+PrintGCDetails -jar dacapo-9.12-MR1-bach.jar ${option}
 	done
 done
